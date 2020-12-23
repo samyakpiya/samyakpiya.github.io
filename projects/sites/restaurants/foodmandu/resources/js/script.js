2521831ecabf681bd4fef8499e060a5c52a9b846
Samyak Piya <samyakpiya@gmail.com>
2020-12-23 15:08:06 +0545

    first commit

---
+++ projects/sites/restaurants/foodmandu/resources/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  // For sticky navigation
+
+  $(".js--section-features").waypoint(
+    function (direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky");
+      } else {
+        $("nav").removeClass("sticky");
+      }
+    },
+    {
+      offset: "72px;",
+    }
+  );
+
+  // Animations on scroll
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animate__animated animate__bounceIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animate__animated animate__fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animate__animated animate__fadeInLeft");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animate__animated animate__pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  // Mobile Nav
+
+  $(".js--nav-icon").click(function () {
+    var nav = $(".js--main-nav");
+    var icon = $(".js--nav-icon ion-icon");
+    nav.slideToggle(200);
+  });
+});
